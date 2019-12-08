db2c9c1d692783b7c6afa569f6e7aef7f371fbb8
Shengting Shao <shsh@umich.edu>
2019-12-07 21:16:31 -0500

    hybridizeremove

--- yolo/train_yolo3.py
+++ yolo/train_yolo3.py
@@ -244,7 +244,7 @@ def train(net, train_data, val_data, eval_metric, ctx, args):
         tic = time.time()
         btic = time.time()
         mx.nd.waitall()
-        # net.hybridize()
+        net.hybridize()
         for i, batch in enumerate(train_data):
             batch_size = batch[0].shape[0]
             data = gluon.utils.split_and_load(batch[0], ctx_list=ctx, batch_axis=0)
